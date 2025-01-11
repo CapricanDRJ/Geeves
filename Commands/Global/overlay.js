d7814c3c16e648eebc222721ba00a9328f6c260c
CapricanDRJ <113147501+CapricanDRJ@users.noreply.github.com>
2025-01-11 03:33:00 +0000

    Fixed overlay.js to work with Jimp 1.6

--- Commands/Global/overlay.js
+++ Commands/Global/overlay.js
@@ -14,22 +14,8 @@ module.exports = {
             .setDescription("Crop an image of the whitestar to the edge of the actual whitestar borders then upload.")),
     async execute(interaction) {
         console.log('overlay');
-        if(!interaction.channel.viewable) {
-            await interaction.reply({
-                content: 'I have no access to this channel',
-                ephemeral: true
-            });
-            return;
-        }
-        if(!interaction.guild.members.me.permissionsIn(interaction.channel.id).has([PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages])) {
-            await interaction.reply({
-                content: 'Sorry, I am missing permission to post here.',
-                ephemeral: true
-            });
-            return;
-        }
         if (interaction.commandName === 'overlay') {
-            const Jimp = require('jimp');
+            const { Jimp } = require('jimp');
             const attachment = await interaction.options.getAttachment("image");
             // Ensure attachment exists and check file size (less than 20MB)
             if (!attachment) {
@@ -39,7 +25,7 @@ module.exports = {
                 });
                 return;
             }
-            
+console.log("attachment size "+attachment.size);
             if (attachment.size > 20 * 1024 * 1024) {
                 await interaction.reply({
                     content: "The file is too large. Please upload an image smaller than 20MB.",
@@ -51,15 +37,17 @@ module.exports = {
 		await interaction.deferReply();
                 if (attachment.hasOwnProperty('url') && attachment.hasOwnProperty('name')) {
                     const WSimages = [];
-                    WSimages.push(Jimp.read(attachment.url));
-                    WSimages.push(Jimp.read('./files/wsOverlay.png'));
+                    WSimages.push(await Jimp.read(attachment.url));
+                    WSimages.push(await Jimp.read('./files/wsOverlay.png'));
                     await Promise.all(WSimages).then(function(data) {
                         return Promise.all(WSimages);
                     }).then(async function(data) {
-                        await data[1].resize(attachment.width, attachment.height);
+                        await data[1].resize({ w: attachment.width, h: attachment.height });
                         await data[0].composite(data[1], 0, 0);
+    const buffer = await data[0].getBuffer(data[0].mime);
+
                         let att = await new AttachmentBuilder()
-                            .setFile(await data[0].getBufferAsync(Jimp.MIME_PNG))
+                            .setFile(buffer)
                             .setName("Whitestar.png")
                             .setDescription("Overlay of Whitestar");
 
@@ -76,7 +64,7 @@ module.exports = {
                 };
             } catch (error) {
                 console.error(error);
-                await interaction.EditReply({
+                await interaction.editReply({
                     content: "There was an error processing the image. Please make sure you uploaded a valid image file.",
                     ephemeral: true
                 });
