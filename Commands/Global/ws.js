d7102fa03e4b9fdf943c48f1405a2c4da2d5857b
Caprican <CapricanDRJ@users.noreply.github.com>
2025-05-12 01:27:53 -0400

    cleanup.

--- Commands/Global/ws.js
+++ Commands/Global/ws.js
@@ -385,19 +385,15 @@ module.exports = {
         if (!interaction.channel.isTextBased()) return;
         if (interaction.isAutocomplete()) {
             if (interaction.options.getSubcommand() === 'new' && interaction.options.getFocused(true)?.name === 'corp') {
-console.log("autocomplete for corp");
                 const guildId = interaction.guildId;
                 if (!corpCacheMap.has(guildId)) {
                     loadCorpCacheForGuild(guildId);
                 }
-console.log("cacheMap has guildId");
                 const focused = interaction.options.getFocused().toLowerCase();
                 const entries = corpCacheMap.get(guildId)?.data || [];
                 const filtered = entries.filter(entry =>
                     entry.name.toLowerCase().includes(focused)
                 ).slice(0, 25);
-                console.log("filtered is " + filtered);
-                console.log(filtered);
                 interaction.respond(filtered).catch(console.error);
             }
             
@@ -961,7 +957,7 @@ console.log("cacheMap has guildId");
                 async function startWS() {
                     const corpOption = interaction.options.getString('corp');
                     if (corpOption !== 'NoAutoSetup' && !/^[a-f0-9]{64}\|[01]$/.test(corpOption)) {
-                      return interaction.editReply('Invalid input.');
+                      return interaction.editReply('Invalid Corp.');
                     }
                     const [corpId, slot] = corpOption === 'NoAutoSetup' ? [null, null] : corpOption.split('|');
 
